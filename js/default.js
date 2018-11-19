a2ae48423375d1239ce395d48eb647b912a69843
yu-ji165 <38420679+yu-ji165@users.noreply.github.com>
2018-11-19 16:22:13 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,1 +1,22 @@
-console.log('おつおつおー');
+var number = Math.floor(Math.random()*3);
+ver ans = parseInt(window.prompt('数を当てて見せろ、０～２だ。'));
+
+var mes;
+if(ans === number)
+{
+  mes = 'あたりだ'; 
+}
+else if(ans < number)
+{
+  mes = 'ばかめ！もっと大きいわ！'; 
+}
+else if(ans > number)
+{
+  mes = 'ばかめ！もっと小さいわ！'; 
+}
+else
+{
+  mes = 'お前は馬鹿か？'; 
+}
+
+document.getElementById('choice').textContent = mes;
